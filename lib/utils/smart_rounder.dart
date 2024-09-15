808650c8ce232d6591950ba1be6f814820028b06
David Yu <davidy@kth.se>
2024-09-16 01:39:44 +0200

    shorter and more effective rounding function

--- lib/utils/smart_rounder.dart
+++ lib/utils/smart_rounder.dart
@@ -1,27 +1,24 @@
+import 'dart:math';
+
 num smartRound(double value) {
-  String str = value.toString();
-  int decimalIndex = str.indexOf(".");
-  if (decimalIndex == -1) {
-    return double.parse(value.toStringAsPrecision(3)).toInt();
+  if (value == 0.0) return 0;
+
+  double absValue = value.abs();
+  int digits;
+
+  if (absValue >= 1) {
+    digits = (log(absValue) / ln10).floor() + 1;
   } else {
-    var intString = str.substring(0, decimalIndex);
-    var intPartDouble = double.parse(intString);
-    if (intString.length >= 3) {
-      return double.parse(intPartDouble.toStringAsPrecision(3)).toInt();
-    }
-    var intPart = double.parse(intString);
+    digits = 1;
+  }
 
-    var dec = str.substring(decimalIndex, str.length);
-    var dec_double = double.parse(dec);
-    var dec_modified = intPart < 1
-        ? dec_double.toStringAsPrecision(3)
-        : dec_double.toStringAsPrecision(3 - intString.length);
-
-    var doublePart = double.parse(dec_modified);
-
-    var finalDouble = intPart + doublePart;
-    // Format finalDouble to 2 decimal places
-    finalDouble = double.parse(finalDouble.toStringAsFixed(2));
-    return finalDouble;
+  if (digits >= 3) {
+    double factor = pow(10, digits - 3).toDouble();
+    return ( (value / factor).round() * factor ).toInt();
+  } else {
+    int decimalPlaces = 3 - digits;
+    double roundedValue = (value * pow(10, decimalPlaces)).round() / pow(10, decimalPlaces);
+    // Ensure two decimal places as per original function
+    return double.parse(roundedValue.toStringAsFixed(decimalPlaces));
   }
 }
