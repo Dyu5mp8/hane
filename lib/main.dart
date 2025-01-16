8e38748707196087993bea2ee9fb95e289a85f09
David Yu <davidy@kth.se>
2025-01-16 23:37:48 +0100

    fixed issue w splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:hane/startup_errors.dart';
 
-
-
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(
@@ -37,32 +35,33 @@ class _MyAppState extends State<MyApp> {
     super.initState();
     _initialization = initializeFirebase();
   }
-Future<FirebaseApp> initializeFirebase() async {
-  try {
-    // Begin Firebase initialization and delay concurrently
-    final results = await Future.wait([
-      Firebase.initializeApp(
-        options: DefaultFirebaseOptions.currentPlatform,
-      ),
-      // Ensures a minimum of 3 seconds
-      Future.delayed(Duration(seconds: 1)),
-    ]);
 
-    // Extract the FirebaseApp from the results (first item in the list)
-    final app = results[0] as FirebaseApp;
+  Future<FirebaseApp> initializeFirebase() async {
+    try {
+      // Begin Firebase initialization and delay concurrently
+      final results = await Future.wait([
+        Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        ),
+        // Ensures a minimum of 3 seconds
+        Future.delayed(Duration(seconds: 1)),
+      ]);
 
-    // Configure Firestore settings
-    var firestore = FirebaseFirestore.instance;
-    firestore.settings = const Settings(
-      persistenceEnabled: true,
-      cacheSizeBytes: Settings.CACHE_SIZE_UNLIMITED,
-    );
+      // Extract the FirebaseApp from the results (first item in the list)
+      final app = results[0] as FirebaseApp;
 
-    return app;
-  } catch (e) {
-    rethrow; // Propagate errors to be caught by the FutureBuilder
+      // Configure Firestore settings
+      var firestore = FirebaseFirestore.instance;
+      firestore.settings = const Settings(
+        persistenceEnabled: true,
+        cacheSizeBytes: Settings.CACHE_SIZE_UNLIMITED,
+      );
+
+      return app;
+    } catch (e) {
+      rethrow; // Propagate errors to be caught by the FutureBuilder
+    }
   }
-}
 
   @override
   Widget build(BuildContext context) {
@@ -73,7 +72,7 @@ Future<FirebaseApp> initializeFirebase() async {
         Widget homeWidget;
 
         if (snapshot.connectionState == ConnectionState.waiting) {
-         homeWidget = const SplashScreenWidget();
+          homeWidget = Theme(data: theme, child: const SplashScreenWidget());
         } else if (snapshot.hasError) {
           homeWidget = GenericErrorWidget(
             errorMessage: 'Failed to initialize Firebase: ${snapshot.error}',
@@ -88,42 +87,35 @@ Future<FirebaseApp> initializeFirebase() async {
         }
 
         return MultiProvider(
-          providers: [
+            providers: [
+              ChangeNotifierProvider(
+                create: (_) => DrugListProvider(),
+              ),
+              // Other providers...
+            ],
+            child: MaterialApp(
+              title: 'AnestesiH',
+              debugShowCheckedModeBanner: false,
+              theme: theme,
+              home: homeWidget,
+              builder: (context, child) {
+                // Retrieve the current MediaQuery data
+                final MediaQueryData data = MediaQuery.of(context);
 
-            ChangeNotifierProvider(
-              create: (_) => DrugListProvider(),
-            
-            ),
-            // Other providers...
-          ],
-          child:MaterialApp(
-  title: 'AnestesiH',
-  debugShowCheckedModeBanner: false,
-  theme: theme,
-  home: homeWidget,
-  builder: (context, child) {
-    // Retrieve the current MediaQuery data
-    final MediaQueryData data = MediaQuery.of(context);
+                // Clamp the text scale factor to be between 0.8 and 1.2
+                final scale = data.textScaler
+                    .clamp(maxScaleFactor: 1.2, minScaleFactor: 0.8);
 
-    // Clamp the text scale factor to be between 0.8 and 1.2
-    final scale = data.textScaler.clamp(maxScaleFactor: 1.2, minScaleFactor: 0.8);
-
-    // Apply the modified MediaQuery data to the app
-    return MediaQuery(
-      data: data.copyWith(textScaler: scale),
-      child: child!,
+                // Apply the modified MediaQuery data to the app
+                return MediaQuery(
+                  data: data.copyWith(textScaler: scale),
+                  child: child!,
+                );
+              },
+            ));
+      },
     );
-  },
-
-              )
-        );  
-            },
-          );
   }
-        
-   
-  
-  
 }
 
 class AuthGate extends StatelessWidget {
